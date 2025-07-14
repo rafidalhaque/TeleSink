22a765c26505d8b552afb264a1efc87399a6e0e6
rafidalhaque <rafidalhaque@proton.me>
2025-07-14 15:32:30 +0600

    feat: add `start` command
    Signed-off-by: rafidalhaque <rafidalhaque@proton.me>

--- bot/links.py
+++ bot/links.py
@@ -9,3 +9,13 @@ env_path = Path('env') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Assalamu Alaikum!")
+
+
+if __name__ == '__main__':
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
