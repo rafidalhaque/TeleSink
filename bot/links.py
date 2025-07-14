0561ee28d7667604af9cbcf296513584ef3a7289
rafidalhaque <rafidalhaque@proton.me>
2025-07-14 21:27:08 +0600

    feat: add `/help` handler
    Signed-off-by: rafidalhaque <rafidalhaque@proton.me>

--- bot/links.py
+++ bot/links.py
@@ -89,4 +89,5 @@ if __name__ == '__main__':
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("shorten", link_shorten))
+    app.add_handler(CommandHandler("help", help))
     app.run_polling()
